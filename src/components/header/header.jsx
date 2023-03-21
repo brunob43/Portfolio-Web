c077eee9619e6074236ed4e2c68e66c6d097c55f
Bruno mac <brunob43@outlook.com>
2023-03-21 14:23:50 -0300

    foto

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import "./header.css";
+import CTA from "./CTA";
+import bb from "../../assets/bb.png"
 
 const header = () => {
   return (
-    <div>header</div>
-  )
+    <header>
+      <div className="conteiner header__container">
+        <h5>Hello I'm</h5>
+        <h1>Bruno Buglioni</h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+        <CTA/>
+
+        <div className='bb'>
+            <img src={bb} alt="bb" />
+        </div>
+      </div>
+    </header>  )
 }
 
 export default header
