e78834c8cd6ea84ff04b84a7989571e1199862b0
Bruno mac <brunob43@outlook.com>
2023-03-26 17:45:17 -0300

    about

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import "./about.css";
 import BB1 from "../../assets/BB1.PNG";
+import {FaAward} from "react-icons/fa";
+import {FiUsers} from "react-icons/fi";
+import {VscFolderLibrary} from "react-icons/vsc"
 
 const About = () => {
   return (
@@ -19,12 +22,27 @@ const About = () => {
     <div className="about__content">
       <div className="about__cards">
         <article className='about__card'>
+          <FaAward className='about__icon'/>
           <h5>Experience</h5>
           <small>1 Year Working</small>
+        </article>
 
+        <article className='about__card'>
+          <FiUsers className='about__icon'/>
+          <h5>Clients</h5>
+          <small>3 Real Clients </small>
+        </article>
+
+        <article className='about__card'>
+          <VscFolderLibrary className='about__icon'/>
+          <h5>Projects</h5>
+          <small>3 Projects Completed</small>
         </article>
       </div>
-
+      <p>
+        Soy Bruno Buglioni FullStack Developer .................. tecnologias que ulitizo son ....... 
+      </p>
+      <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
     </div>
 
