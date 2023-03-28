642e1614209be5406da51e59f2d2e05cdbd95d75
Bruno mac <brunob43@outlook.com>
2023-03-28 11:58:06 -0300

    footer

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,8 +3,25 @@ import "./contact.css";
 import {MdOutlineEmail} from "react-icons/md"
 import {BsGithub} from "react-icons/bs"
 import {BsLinkedin} from "react-icons/bs"
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_l9aqm4k', 'template_tmc79f7', form.current, 'yqZfB7CKBrspn0AFM')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset();
+  };
+
   return (
     <section id='contact'>
         <h5>Get In Touch</h5>
@@ -34,7 +51,7 @@ const Contact = () => {
             </article>
           </div>
           {/* End of contact Options */}
-          <form action="">
+          <form ref={form} onSubmit={sendEmail}>
             <input type="text" name="name" placeholder='Your Full Name' required />
             <input type="email" name="email" placeholder='Your Email' required />
             <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
