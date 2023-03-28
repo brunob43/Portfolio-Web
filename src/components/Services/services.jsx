f718843a1d746f89eaafdec2017b17f1a99016a0
Bruno mac <brunob43@outlook.com>
2023-03-28 06:22:13 -0300

    services

--- src/components/Services/services.jsx
+++ src/components/Services/services.jsx
@@ -1,10 +1,117 @@
 import React from 'react';
 import "./services.css";
+import  {BiCheck} from "react-icons/bi"
 
 const Services = () => {
   return (
     <section id='services'>
-      
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+      <article className=''>
+        <div className="service__head">
+          <h3>FrontEnd</h3>
+        </div>
+        <ul className='service__list'>
+          <li>
+          <BiCheck className='service__list-icon' /> 
+          <p>Lorem lkdanljnalksjlcnadsn ñlckans</p>
+          </li>
+
+          <li>
+          <BiCheck className='service__list-icon' /> 
+          <p>Lorem lkdanljnalksjlcnadsn ñlckans</p>
+          </li>
+
+          <li>
+          <BiCheck className='service__list-icon' /> 
+          <p>Lorem lkdanljnalksjlcnadsn ñlckans</p>
+          </li>
+
+          <li>
+          <BiCheck className='service__list-icon' /> 
+          <p>Lorem lkdanljnalksjlcnadsn ñlckans</p>
+          </li>
+
+          <li>
+          <BiCheck className='service__list-icon' /> 
+          <p>Lorem lkdanljnalksjlcnadsn ñlckans</p>
+          </li>
+
+        </ul>
+      </article>
+
+     {/* -------------------- END OF FRONT---------- */}
+     <article className=''>
+        <div className="service__head">
+          <h3>BackEnd</h3>
+        </div>
+        <ul className='service__list'>
+          <li>
+          <BiCheck className='service__list-icon' /> 
+          <p>Lorem lkdanljnalksjlcnadsn ñlckans</p>
+          </li>
+
+          <li>
+          <BiCheck className='service__list-icon' /> 
+          <p>Lorem lkdanljnalksjlcnadsn ñlckans</p>
+          </li>
+
+          <li>
+          <BiCheck className='service__list-icon' /> 
+          <p>Lorem lkdanljnalksjlcnadsn ñlckans</p>
+          </li>
+
+          <li>
+          <BiCheck className='service__list-icon' /> 
+          <p>Lorem lkdanljnalksjlcnadsn ñlckans</p>
+          </li>
+
+          <li>
+          <BiCheck className='service__list-icon' /> 
+          <p>Lorem lkdanljnalksjlcnadsn ñlckans</p>
+          </li>
+
+        </ul>
+      </article>
+
+     {/* -------------------- END OF BACK---------- */}
+     <article className=''>
+        <div className="service__head">
+          <h3>Data Base</h3>
+        </div>
+        <ul className='service__list'>
+          <li>
+          <BiCheck className='service__list-icon' /> 
+          <p>Lorem lkdanljnalksjlcnadsn ñlckans</p>
+          </li>
+
+          <li>
+          <BiCheck className='service__list-icon' /> 
+          <p>Lorem lkdanljnalksjlcnadsn ñlckans</p>
+          </li>
+
+          <li>
+          <BiCheck className='service__list-icon' /> 
+          <p>Lorem lkdanljnalksjlcnadsn ñlckans</p>
+          </li>
+
+          <li>
+          <BiCheck className='service__list-icon' /> 
+          <p>Lorem lkdanljnalksjlcnadsn ñlckans</p>
+          </li>
+
+          <li>
+          <BiCheck className='service__list-icon' /> 
+          <p>Lorem lkdanljnalksjlcnadsn ñlckans</p>
+          </li>
+
+        </ul>
+      </article>
+
+     {/* -------------------- END OF DBB---------- */}
+      </div>
     </section>
   )
 }
