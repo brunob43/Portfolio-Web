1551e605beaca9cc3a004fade8febcffc3b2e8f2
Bruno mac <brunob43@outlook.com>
2023-03-28 11:19:41 -0300

    contact3

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -11,23 +11,23 @@ const Contact = () => {
         <h2>Contact Me</h2>
 
         <div className="container contact__container">
-          <div className="contact__option">
+          <div className="contact__options">
             <article className="contact__option">
-              <MdOutlineEmail/>
+              <MdOutlineEmail className='contact__option-icon'/>
               <h4>Email</h4>
               <h5>brunobuglioni43@gmail.com</h5>
               <a href="mailto:brunobuglioni43@gmail.com">Send a message</a>
             </article>
 
             <article className="contact__option">
-              <BsGithub/>
+              <BsGithub className='contact__option-icon'/>
               <h4>GitHub</h4>
               <h5>brunob43</h5>
               <a href="https://github.com/brunob43">Visit Me</a>
             </article>
 
             <article className="contact__option">
-              <BsLinkedin/>
+              <BsLinkedin className='contact__option-icon'/>
               <h4>Linkedin</h4>
               <h5>Bruno Buglioni</h5>
               <a href="https://www.linkedin.com/in/brunobuglioni/">Visit Me</a>
@@ -35,7 +35,10 @@ const Contact = () => {
           </div>
           {/* End of contact Options */}
           <form action="">
-            <input type="text" />
+            <input type="text" name="name" placeholder='Your Full Name' required />
+            <input type="email" name="email" placeholder='Your Email' required />
+            <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+            <button type='submite'className='btn btn-primary'>Send Message</button>
           </form>
         </div>
     </section>
