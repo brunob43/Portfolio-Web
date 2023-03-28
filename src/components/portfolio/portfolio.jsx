235cfcb2205dd423f2e6621a1e01212082466c7d
Bruno mac <brunob43@outlook.com>
2023-03-28 09:07:22 -0300

    contact

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -3,6 +3,23 @@ import "./portfolio.css";
 import IMG1 from "../../assets/IMG1.png"
 import IMG from "../../assets/IMG.png"
 
+const data = [
+ {
+  id: 1,
+  image: IMG1,
+  title: "Henry Game On",
+  github: "https://github.com",
+  demo: "https://youtube.com"
+ },
+ {
+  id: 2,
+  image: IMG,
+  title: "Earth App",
+  github: "https://github.com",
+  demo: "https://youtube.com"
+ }
+]
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
@@ -10,24 +27,25 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-    <article className='portfolio__item'>
-    <div className='portfolio__item-image'>
-    <img src={IMG1} alt="" />
-    </div>
-    <h3>This is a portfolio item title</h3>
-    <a href="https://github.com" className='btn' target="_blank">Github</a>
-    <a href="https://youtube.com" className='btn btn-primary' target="_blank">Live Demo</a>
-    </article>
+      {
+        data.map(({id, image, title, github, demo}) => {
+        return (
+          <article key={id} className='portfolio__item'>
+           <div className='portfolio__item-image'>
+              <img src={image} alt={title} />
+           </div>
+         <h3>{title}</h3>
+           <div className="portfolio__item-cta"> 
+              <a href={github} className='btn' target="_blank">Github</a>
+              <a href={demo} className='btn btn-primary' target="_blank">Live Demo</a>
+          </div>
+         </article>
+      )
+    })
+   }
 
-    <article className='portfolio__item'>
-    <div className='portfolio__item-image'>
-    <img src={IMG} alt="" />
-    </div>
-    <h3>This is a portfolio item title</h3>
-    <a href="https://github.com" className='btn' target="_blank">Github</a>
-    <a href="https://youtube.com" className='btn btn-primary' target="_blank">Live Demo</a>
-    </article>
-    </div>
+ 
+      </div>
     </section>
   )
 }
